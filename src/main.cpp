a1ad6803b6a8682c6df5ae8b7044d1ab143c9df7
redve <106469071+redve-dev@users.noreply.github.com>
2022-08-05 15:45:20 +0200

    remove unused func

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include "InputData/InputData.hpp"
 #include "Item/Item.hpp"
-#include "functions.hpp"
-#include <iomanip>
 #include <iostream>
 
 int main() {
-  InputData data("items");
-  data.Update();
+   InputData data("items");
+   data.Update();
 }

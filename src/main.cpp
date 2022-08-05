5970f75fd813d9a546950c8e6fe59b3b4924ba20
redve <106469071+redve-dev@users.noreply.github.com>
2022-08-05 16:32:44 +0200

    format code

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char **args) {
 	std::string file = "items";
 	if (argc > 0) {
 		file = args[1];
-		if (file.find_first_not_of(' ') == std::string::npos){
-			file="items";
+		if (file.find_first_not_of(' ') == std::string::npos) {
+			file = "items";
 		}
 	}
 	InputData data(file);

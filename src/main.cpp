e2d5c63abde62502857e6f287740825f61cec9ae
redve <106469071+redve-dev@users.noreply.github.com>
2022-08-05 16:24:30 +0200

    add cli argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include "InputData/InputData.hpp"
 #include "Item/Item.hpp"
 #include <iostream>
+#include <string>
 
-int main() {
-	InputData data("items");
+int main(int argc, char **args) {
+	std::string file = "items";
+	if (argc > 0) {
+		file = args[1];
+	}
+	InputData data(file);
 	data.Update();
 }

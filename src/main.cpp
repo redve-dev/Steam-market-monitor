cc03e137de7910150c56618a4c3297f2bb2c40a9
redve <106469071+redve-dev@users.noreply.github.com>
2022-08-05 16:31:41 +0200

    sanitize input

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@ int main(int argc, char **args) {
 	std::string file = "items";
 	if (argc > 0) {
 		file = args[1];
+		if (file.find_first_not_of(' ') == std::string::npos){
+			file="items";
+		}
 	}
 	InputData data(file);
 	data.Update();
